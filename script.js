8e756cae12f3e13322476bd4337191b4c8aab19e
student <89061703+arannyakarak@users.noreply.github.com>
2022-02-18 12:08:40 -0800

    add positon value in action

---
+++ script.js
@@ -0,0 +1,14 @@
+const text  = document.querySelector('.mainText');
+
+const mainContainer  = document.querySelector('.container');
+
+mainContainer.addEventListener("mousemove", runEvent);
+//event handler
+function runEvent(e){
+    e.preventDefault();
+    
+    //print Coordinates of the mouse on move on the targeted element: 
+    text.textContent = `Mouse-moveX: ${e.offsetX},  Mouse-moveY: ${e.offsetY}`;
+    //change body background color taking the coordinates as values of rgb:
+   // document.body.style.backgroundColor = `rgb(${e.offsetX}, 180, ${e.offsetY})`;
+}
